9b93efbd8d55e4ef2ccf44616791e4a5718cb86d
Enzo <esucomine@gmail.com>
2026-03-10 20:53:24 -0300

    2º commit

--- web/src/app/(frontend)/layout.tsx
+++ web/src/app/(frontend)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import { ToastProvider } from "@/components/common/ToastProvider";
 import "./global.css";
+import Link from "next/link";
 
 const rubik = Rubik({
   variable: "--font-rubik",
@@ -22,9 +23,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <nav className="flex flex-col items-center gap-2 p-4 bg-black">
           <h2 className="text-center text-4xl font-semibold text-white">DinOvo</h2>
           <div className="flex gap-4 text-white">
-            <a href="/">Home</a>
-            <a href="/produtos">Produtos</a>
-            <a href="/sobre">Sobre</a>
+            <link href="/">Home</link>
+            <link href="/produtos">Produtos</link>
+            <link href="/sobre">Sobre</link>
           </div>
         </nav>
 
